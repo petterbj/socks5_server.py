a9b3c7bfe67ff20c86020b453d1bce856286a48d
Petter Shi <petterbj@yahoo.com>
2016-10-18 07:42:40 -0700

    Updated socks5.py

--- socks5.py
+++ socks5.py
@@ -1,6 +1,13 @@
 import socket
 import struct
 import select
+def send_tcp_to_each_other(client_server_socket, server_remote_socket):
+    while True:
+        r, w, e = select.select([server_remote_socket,client_server_socket],[],[])
+        if server_remote_socket in r:
+            if client_server_socket.send(server_remote_socket.recv(4096)) <= 0:break
+        if client_server_socket in r:
+            if server_remote_socket.send(client_server_socket.recv(4096)) <= 0:break
 def request_form_filler(data):
     form_pointer = 0
     socks_request = {}
@@ -66,6 +73,9 @@ while True:
         server_remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         print remote_addr, remote_port
         server_remote_socket.connect((remote_addr,remote_port))
+    elif socks_request['CMD'] == '\x02':
+    elif socks_request['CMD'] == '\x03':
+    else:
 
     # VER | REP |  RSV  | ATYP | BND.ADDR | BND.PORT
     reply = b'\x05\x00\x00\x01'
@@ -74,13 +84,8 @@ while True:
     reply += struct.pack('>H',server_remote_socket.getsockname()[1])
 
     client_server_socket.send(reply)
+    send_tcp_to_each_other(client_server_socketection, server_remote_socket)
 
-    while True:
-        r, w, e = select.select([server_remote_socket,client_server_socket],[],[])
-        if server_remote_socket in r:
-            if client_server_socket.send(server_remote_socket.recv(4096)) <= 0:break
-        if client_server_socket in r:
-            if server_remote_socket.send(client_server_socket.recv(4096)) <= 0:break
     print 'client_server_socketection closed'
 #server_socket.close()
 
